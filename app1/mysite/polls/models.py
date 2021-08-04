c963b2c538fa3e431206727cf27b1da4fbe01534
akshar <bhalani.akshar1277@gmail.com>
2021-08-04 18:11:40 +0530

    create models

--- app1/mysite/polls/models.py
+++ app1/mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+
+class Choice(models.Model):
+    Question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    votes=models.ImageField(default=0)
+    
