9b6b35510cb039d47f9a32bb1ac4d3ca6e547f35
akshar <bhalani.akshar1277@gmail.com>
2021-08-06 13:01:17 +0530

    generitview

--- app1/mysite/polls/views.py
+++ app1/mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
+from django.urls import reverse
+from django.shortcuts import get_object_or_404,render
 # Create your views here.
 
 def index(request):
@@ -16,21 +18,36 @@ def index(request):
 
 
 def detail(request,question_id):
-    try:
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-
+    # try:
+    #     question=Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Quetstion does not exist")
+    question=get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':question})
-    
+
 
 def results(request,question_id):
-    response="you are looking at the results of quations %s"
-
-    return HttpResponse(response % question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/result.html',{'question': question})
 
 def vote(request,question_id):
-    return HttpResponse("you're voting on question %s" % question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':"you didnt select a choice",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
+
+
+
+
+
+    
 
